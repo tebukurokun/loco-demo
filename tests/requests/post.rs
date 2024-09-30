cb411835bc875ae0904b1dfd55418c8ebf970a3b
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2024-09-30 19:49:17 +0900

    first commit

---
+++ tests/requests/post.rs
@@ -0,0 +1,16 @@
+use loco_demo::app::App;
+use loco_rs::testing;
+use serial_test::serial;
+
+#[tokio::test]
+#[serial]
+async fn can_list() {
+    testing::request::<App, _, _>(|request, _ctx| async move {
+        let res = request.get("/post/").await;
+        assert_eq!(res.status_code(), 200);
+
+        // you can assert content like this:
+        // assert_eq!(res.text(), "content");
+    })
+    .await;
+}
